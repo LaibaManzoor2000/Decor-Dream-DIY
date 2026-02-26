8e4ae15cd3adee6752e449f3b2aa11610fe79d30
ismail6385 <76931576+ismail6385@users.noreply.github.com>
2026-02-27 00:13:15 +0500

    Fix: Remove src cache

--- src/app/not-found.js
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-    return (
-        <div style={{ padding: '50px', textAlign: 'center' }}>
-            <h1>Custom Next.js App 404 Page</h1>
-            <p>If you see this, Next.js is running, but the specific route was not found in your Next.js code.</p>
-        </div>
-    );
-}
